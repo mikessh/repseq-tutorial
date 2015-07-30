6c4d53cf5541392e8da7da1909bf7de029fb4a53
mikesh <mikhail.shugay@gmail.com>
2015-07-30 17:21:08 +0100

    Fix part2 shell script

--- part2/run.sh
+++ part2/run.sh
@@ -1,2 +1,5 @@
-java -Xmx8G -jar ../vdjtools-1.0.2.jar $VDJTOOLS CalcDiversityStats -m metadata.txt .
+# Compute diversity statistics
+java -Xmx8G -jar ../vdjtools-1.0.2.jar CalcDiversityStats -m metadata.txt .
+
+# Compare various estimates using correlation with age and %naive T-cells
 Rscript plot.R
